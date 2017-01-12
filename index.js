e2fed2f2a6bd6480ebe3f4dfec588e3894b5772e
Lisa Thomas <lisathomas@Lisas-MacBook-Pro.local>
2017-01-12 11:21:02 -0800

    mortgage

---
+++ index.js
@@ -0,0 +1,61 @@
+function monthlyPayment() {
+    //gets the values from user input and select
+    var rate = document.getElementById("interestRate").value;
+    var loanBalance = document.getElementById("loanBalance").value;
+    var term = document.getElementById("loanTerm").value;
+    var period = document.getElementById("period").value;
+    // var periodNumber = period.option[period.selectedIndex].value;
+
+
+    //equation for monthly payments 
+    //console.log to check to see if every var is working correctly
+    var monthlyInterestRate = (rate / 100) / period;
+    //console.log(monthlyInterestRate);
+
+    var numberOfPayments = term * period;
+    //console.log(numberOfPayments);
+
+    var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments);
+    //console.log(compoundedInterestRate);
+
+    var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+    //console.log(interestQuotient);
+
+    var totalMonthlyPayment = parseInt(loanBalance * interestQuotient);
+    //console.log(totalMonthlyPayment);
+
+    console.log(totalMonthlyPayment);
+
+  
+
+    var printTotal = "You pay $" + totalMonthlyPayment + " per period";
+    console.log(printTotal);
+
+    document.getElementById("paragraph").innerHTML = printTotal;
+
+
+    
+   
+   
+}
+
+
+
+
+
+
+
+// // monthly interest rate       
+// var monthlyInterestRate = (interestRate / 100) / period          
+
+// // number of payments
+// numberOfPayments = loanTerm * period
+
+// // compounded interest rate
+// compoundedIntestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments)  
+
+// // interest quotient
+// interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedIntestRate - 1)
+
+// // final calculation
+// monthlyPayment = loanBalance * interestQuotient;
