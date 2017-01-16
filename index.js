5712af184b0cdcd2e64841f728f1d1ada7372ab4
Lisa Thomas <lisathomas@Lisas-MacBook-Pro.local>
2017-01-16 11:16:53 -0800

    new

--- index.js
+++ index.js
@@ -33,29 +33,6 @@ function monthlyPayment() {
 
     document.getElementById("paragraph").innerHTML = printTotal;
 
-
-    
-   
    
 }
 
-
-
-
-
-
-
-// // monthly interest rate       
-// var monthlyInterestRate = (interestRate / 100) / period          
-
-// // number of payments
-// numberOfPayments = loanTerm * period
-
-// // compounded interest rate
-// compoundedIntestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments)  
-
-// // interest quotient
-// interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedIntestRate - 1)
-
-// // final calculation
-// monthlyPayment = loanBalance * interestQuotient;
